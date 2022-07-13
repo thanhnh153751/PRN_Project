16de4ed0799f0ef17d09ddb5a6c36615a95bf507
thanhnhhe153751 <thanhnhhe153751@fpt.edu.vn>
2022-07-13 13:19:36 +0700

    update_home2

--- Project_ASP.NET_ShoppingOnline/Controllers/ProductController.cs
+++ Project_ASP.NET_ShoppingOnline/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Project_ASP.NET_ShoppingOnline.Logics;
 using Project_ASP.NET_ShoppingOnline.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Project_ASP.NET_ShoppingOnline.Controllers
 {
@@ -12,13 +13,27 @@ namespace Project_ASP.NET_ShoppingOnline.Controllers
             return View();
         }
 
+        //[HttpGet]
+        //public JsonResult GetProductByCategoryId(int id)//id ở đây là categoryId
+        //{
+        //    ProductManager productManager = new ProductManager();
+        //    List<Product> products = productManager.GetProductsByCategoryId(id);
+            
+        //    return Json(new { code = 666,listProduct = products,cid=id, msg = "successfuly" });
+        //}
+
         [HttpGet]
-        public JsonResult GetProductByCategoryId(int id)//id ở đây là categoryId
+        public JsonResult GetProductByCategoryId(int id, int page = 1)//id ở đây là categoryId
         {
             ProductManager productManager = new ProductManager();
             List<Product> products = productManager.GetProductsByCategoryId(id);
-            
-            return Json(new { code = 666,listProduct = products,cid=id, msg = "successfuly" });
+
+            const int PageSize = 8;
+            var count = products.Count();
+            var data = products.Skip((page - 1) * PageSize).Take(PageSize).ToList();
+            var MaxPage = (count / PageSize) - (count % PageSize == 0 ? 1 : 0);
+
+            return Json(new { code = 666, listProduct = data, cid = id,page = page,maxpage = MaxPage, msg = "successfuly" });
         }
     }
 }
